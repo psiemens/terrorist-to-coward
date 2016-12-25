5f41b528a938b345976bf4d58cb7993267ee137b
U-CR-PC-04\Lahiru Himesh <lahiru.lahirumadusanka@gmail.com>
2016-12-25 16:50:27 +0530

    Move translation to seperate file
    Moved translation strings to a seperate file for easy update. And to prevent any unintended script changes.

---
+++ translation.js
@@ -0,0 +1,70 @@
+var REPLACEMENTS = {
+    // English
+    'terrorist': 'coward',
+    'Terrorist': 'Coward',
+    'terrorism': 'cowardice',
+    'Terrorism': 'Cowardice',
+    // Portuguese
+    'terrorista': 'covarde',
+    'Terrorista': 'Covarde',
+    'terrorismo': 'covardia',
+    'Terrorismo': 'Covardia',
+    // Spanish
+    'terrorista': 'cobarde',
+    'Terrorista': 'Cobarde',
+    'terrorismo': 'cobardía',
+    'Terrorismo': 'Cobardía',
+    //German
+    'Terrorist': 'Feigling',
+    'Terroristen': 'Feiglinge',
+    'Terrorismus': 'Feigheit',
+    'Terror': 'Feige',
+    // Chinese
+    '恐怖': '懦弱',
+    // Turkish
+    'terörist': 'korkak',
+    'Terörist': 'Korkak',
+    'terörizm': 'korkaklık',
+    'Terörizm': 'Korkaklık',
+    'teröristler': 'korkaklar',
+    'Teröristler': 'Korkaklar',
+    //Greek
+    'τρομοκράτης': 'δειλός',
+    'τρομοκρατης': 'δειλός',
+    'τρομοκρατες': 'δειλοί',
+    'Τρομοκρατες': 'Δειλοί',
+    'τρομοκράτες': 'δειλοί',
+    'Τρομοκράτες': 'Δειλοί',
+    'Τρομοκράτης': 'Δειλός',
+    'Τρομοκρατης': 'Δειλός',
+    'τρομοκρατία': 'δειλία',
+    'τρομοκρατια': 'δειλία',
+    'Τρομοκρατία': 'Δειλία',
+    'Τρομοκρατια': 'Δειλία',
+    // Serbian (Latin)
+    'terorista': 'kukavica',
+    'Terorista': 'Kukavica',
+    'terorizam': 'kukavičluk',
+    'Terorizam': 'Kukavičluk',
+    'teroristički': 'kukavičlučki',
+    'Teroristički': 'kukavičlučki',
+    // Serbian (Cyrilic)
+    'терориста': 'кукавица',
+    'Терориста': 'Кукавица',
+    'тероризам': 'кукавичлук',
+    'Тероризам': 'Кукавичлук',
+    'терористички': 'кукавичлучки',
+    'Терористички': 'Кукавичлучки',
+    // Italian
+    'terrorista': 'codardo',
+    'Terrorista': 'Codardo',
+    'terroristi': 'codardi',
+    'Terroristi': 'Codardi',
+    'terrorismo': 'codardia',
+    'Terrorismo': 'Codardia',
+    // Sinhala
+    'ත්‍රස්ථවාදියා' : 'බයගුල්ලා',
+    'ත්‍රස්ථවාදීන්' : 'බයගුල්ලන්',
+    'ත්‍රස්ථවාදී' : 'බියගුලු',
+    'ත්‍රස්තවාදය' : 'බියසුලුකම',
+};
